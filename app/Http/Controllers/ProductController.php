ed98f4a454bdc485e457900907fbbbd811abe804
QuocLe <minhquoc7a3a@gmail.com>
2024-03-21 02:13:27 +0700

    fix not fount price for 5 scanner

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 require_once '../vendor/autoload.php';
 use Goutte\Client;
+use DOMDocument;
+use DOMXPath;
+use Exception;
             
 
 class ProductController extends Controller
@@ -13,7 +16,7 @@ class ProductController extends Controller
     
     public function index(Request $request)
     {
-        $product_checked=Product::all();
+        $product_checked=$this->checkPrice(Product::all());
         return view('product.add_product', [
             'products' => $product_checked
         ]);
@@ -21,7 +24,7 @@ class ProductController extends Controller
 
 
 
-    public function checkPrice($products)
+    private function checkPrice($products)
         {
             foreach ($products as $product) {
                 $product->ab_beautyworld = $this->abScanner($product->ab_beautyworld);
@@ -36,39 +39,85 @@ class ProductController extends Controller
         private function hasakiScanner($value)
         {
             $client = new Client();
+        try {
             $crawler = $client->request('GET', $value);
             $finalPrice = $crawler->filter('#product_final_price')->attr('value');
+        } catch (Exception $e) {
+            return "liên kêt không tồn tại hoặc sai định dạng";
+        }
             return $finalPrice;
         }
 
         private function guScanner($value)
-        {
-            $client = new Client();
-            $crawler = $client->request('GET', $value);
-            $finalPrice = $crawler->filter('span.price')->attr('value');
-            return $finalPrice;
+        { 
+           
+            $dom = new DOMDocument;
+
+            libxml_use_internal_errors(true);
+            try {
+            $dom->loadHTML(file_get_contents($value));
+        } catch (Exception $e) {
+            return "liên kêt không tồn tại hoặc sai định dạng";
+        }
+            libxml_clear_errors();
+    
+            $xpath = new DOMXPath($dom);
+            $prices = $xpath->query("//span[@class='price']");
+            $price = $prices->item(0)->nodeValue;
+            return $price;
+
         }
         private function tgScanner($value)
         {
-            $client = new Client();
-            $crawler = $client->request('GET', $value);
-            $finalPrice = $crawler->filter('span.page-product-info-newprice span')->attr('value');
-            return $finalPrice;
+        $dom = new DOMDocument;
+
+        libxml_use_internal_errors(true);
+        try {
+        $dom->loadHTML(file_get_contents($value));
+         } catch (Exception $e) {
+        return "liên kêt không tồn tại hoặc sai định dạng";
+         }
+        libxml_clear_errors();
+
+        $xpath = new DOMXPath($dom);
+        $prices = $xpath->query("//div[@class='page-product-info-newprice']");
+        $price = $prices->item(0)->nodeValue;
+        
+        return $price;
         }
-        private function ltScanner($link)
-        {
-            $client = new Client();
-            $crawler = $client->request('GET', $link);
-            $price = $crawler->filter('span.ProductPrice');
-            return $price->text();
-            
+           
+      
+        private function ltScanner($value)
+        { 
+        $dom = new DOMDocument;
+        
+        libxml_use_internal_errors(true);
+        try {
+        $dom->loadHTML(file_get_contents($value));}
+        catch (Exception $e) {
+            return "liên kêt không tồn tại hoặc sai định dạng";
+             }
+        libxml_clear_errors();
+
+        $xpath = new DOMXPath($dom);
+        $prices = $xpath->query("//span[@class='current-price ProductPrice']");
+        $price = $prices->item(0)->nodeValue; // Remove the trailing text from the price
+
+        $price = $price;
+        
+        return $price;        
         }
         private function abScanner($link)
         {
             $client = new Client();
+            try {
             $crawler = $client->request('GET', $link);
             $price = $crawler->filter('span.pro-price');
-            return $price->text();
+            
+        } catch (Exception $e) {
+            // Handle the exception here
+            return "liên kêt không tồn tại hoặc sai định dạng";
+        }return $price->text();
             
         }
 
