fc0cf89c095e0bbbd0b5dbcffd86babb09a2f0e9
QuocLe <minhquoc7a3a@gmail.com>
2024-03-20 22:45:52 +0700

    config store funtion to save product infor

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function store(Request $request)
+{
+    $validatedData = $request->validate([
+        'product_barcode' => 'required|max:255',
+        'brand' => 'required|max:255',
+        'product_name' => 'required|max:255',
+        'ab_beautyworld' => 'required|max:255',
+        'hasaki' => 'required|max:255',
+        'guardian' => 'required|max:255',
+        'thegioiskinfood' => 'required|max:255',
+        'lamthao' => 'required|max:255',
+    ]);
+
+    $product = Product::create($validatedData);
+
+    return redirect()->route('products.index')->with('success', 'Product created successfully.');
+}
+}
+
+
